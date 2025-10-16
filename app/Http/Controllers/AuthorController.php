5fc5faeb4fdb07ba6a6a6f61c15d4858a00e89b1
Muhammad Nazwaly <sukrisnanazwaly@gmail.com>
2025-10-16 16:25:18 +0700

    tugas 3projek API

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -7,10 +7,13 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-    public function index(){
-        $authors = Author::all();
+      public function index(){
+        $authors =Author::all();
 
-        return view('authors', ['authors'=>$authors]);
-    
+        return response()->json([
+            "success"=> true,
+            "message"=> "get all resources",
+            "data"=> $authors
+        ],200);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,7 @@
 <?php
 
-use App\Http\Controllers\AuthorController;
-use App\Http\Controllers\BookController;
-use App\Http\Controllers\GenreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/books', function () {
-    return view('books');
-});
-Route::get('/genres', function () {
-    return view('genres');
-});
-Route::get('/authors', function () {
-    return view('authors');
-});
-
-route::get('/books', [BookController::class, 'index']);
-route::get('/genres', [GenreController::class, 'index']);
-route::get('/authors', [AuthorController::class, 'index']);
